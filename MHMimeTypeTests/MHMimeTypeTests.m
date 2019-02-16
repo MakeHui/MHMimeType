d95913105153b29e59ab5452cb70b1728ea40bbf
MakeHui <makehuir@gmail.com>
2019-02-16 11:04:12 +0800

    add fixtures source

--- MHMimeTypeTests/MHMimeTypeTests.m
+++ MHMimeTypeTests/MHMimeTypeTests.m
@@ -16,6 +16,7 @@
 @implementation MHMimeTypeTests
 
 - (void)testExample {
+    // Fixtures from: https://github.com/sendyhalim/Swime/tree/master/Tests/SwimeTests/fixtures
     NSString *filesDir = @"/Users/imacbook/Developer/MHMimeType/MHMimeTypeTests/fixtures";
     NSArray * dirArray = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:filesDir error:nil];
     for (NSString *file in dirArray) {
@@ -28,6 +29,9 @@
 
 - (void)checkFileWithPath:(NSString *)path ext:(NSString *)ext
 {
+    if ([ext isEqualToString:@"tif"]) {
+        NSLog(@"%@", @"tif");
+    }
     MHMimeTypeModel *model = [[MHMimeType sharedInstance] mimeTypeModelWithPath:[NSString stringWithFormat:@"file://%@", path]];
     NSLog(@"%@", [NSString stringWithFormat:@"file ext: %@", ext]);
     XCTAssertTrue([model.ext isEqualToString:ext], @"warn");
